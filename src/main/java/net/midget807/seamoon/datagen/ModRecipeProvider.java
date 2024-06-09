d5b61f3e44c55bf8f28a61804a88ce1b14ae5314
Midget807 <phu7mai@gmail.com>
2024-06-10 01:59:15 +0930

    Added Recipes for Sea Moon Blocks

--- src/main/java/net/midget807/seamoon/datagen/ModRecipeProvider.java
+++ src/main/java/net/midget807/seamoon/datagen/ModRecipeProvider.java
@@ -2,10 +2,12 @@ package net.midget807.seamoon.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.midget807.seamoon.block.ModBlocks;
 import net.midget807.seamoon.item.ModItems;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.*;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
 
@@ -26,6 +28,35 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(Items.GLASS_BOTTLE), conditionsFromItem(Items.GLASS_BOTTLE))
                 .criterion(hasItem(Items.BUCKET), conditionsFromItem(Items.BUCKET))
                 .offerTo(exporter, new Identifier(getRecipeName(ModItems.SEAMOON_BUCKET)));
+
+        createStairsRecipe(ModBlocks.SEAMOON_STAIR, Ingredient.ofItems(ModBlocks.SEAMOON_BLOCK));
+
+        offerSlabRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SEAMOON_SLAB, ModBlocks.SEAMOON_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.SEAMOON_POST, 8)
+                .pattern("S")
+                .pattern("S")
+                .input('S', ModBlocks.SEAMOON_BLOCK)
+                .criterion(hasItem(ModBlocks.SEAMOON_BLOCK), conditionsFromItem(ModBlocks.SEAMOON_BLOCK))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.SEAMOON_POST)));
+
+        createFenceRecipe(ModBlocks.SEAMOON_FENCE, Ingredient.ofItems(ModBlocks.SEAMOON_BLOCK));
+
+        createFenceGateRecipe(ModBlocks.SEAMOON_FENCE_GATE, Ingredient.ofItems(ModBlocks.SEAMOON_BLOCK));
+
+        offerWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SEAMOON_WALL, ModBlocks.SEAMOON_BLOCK);
+
+        createDoorRecipe(ModBlocks.SEAMOON_FENCE, Ingredient.ofItems(ModBlocks.SEAMOON_BLOCK));
+
+        createTrapdoorRecipe(ModBlocks.SEAMOON_FENCE, Ingredient.ofItems(ModBlocks.SEAMOON_BLOCK));
+
+        offerPressurePlateRecipe(exporter, ModBlocks.SEAMOON_PRESSURE_PLATE, ModBlocks.SEAMOON_BLOCK);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.SEAMOON_BUTTON)
+                .input(ModBlocks.SEAMOON_BUTTON)
+                .criterion(hasItem(ModBlocks.SEAMOON_BLOCK), conditionsFromItem(ModBlocks.SEAMOON_BLOCK))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.SEAMOON_BUTTON)));
+
 
     }
 }
